4f30febe7688df3043923bcdfaf4b7726ed0bfd7
xiaoo_gan <85265900@qq.com>
2015-10-23 22:11:06 +0800

    初始化项目

---
+++ TudoraTests/TudoraTests.swift
@@ -0,0 +1,36 @@
+//
+//  TudoraTests.swift
+//  TudoraTests
+//
+//  Created by xiaoo_gan on 10/17/15.
+//  Copyright © 2015 xiaoo_gan. All rights reserved.
+//
+
+import XCTest
+@testable import Tudora
+
+class TudoraTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
