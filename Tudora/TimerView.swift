110df9d94d6b643a01c1e06a1a2f36a947ff003c
xiaoo_gan <85265900@qq.com>
2015-10-27 23:02:40 +0800

    add TimerView.swift

---
+++ Tudora/TimerView.swift
@@ -0,0 +1,21 @@
+//
+//  TimerView.swift
+//  Tudora
+//
+//  Created by xiaoo_gan on 10/27/15.
+//  Copyright © 2015 xiaoo_gan. All rights reserved.
+//
+
+import UIKit
+
+class TimerView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
